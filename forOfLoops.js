c1d9184c9a3ffc39656d2182e0a54de1941b6b54
MahmudurNishat <rajbiroy23@gmail.com>
2024-03-26 18:09:51 +0600

    for of loops added

--- forOfLoops.js
+++ forOfLoops.js
@@ -1,17 +1,11 @@
 
-        Know the for of loops  <Start>
-=================================================
-/// * intro loop  <br>
-// * it means same work will continued once after another until you stop it  <br>
-/// * ekta jinis bar bar chote thaka  <br>
+/// * intro loop 
+// * it means same work will continued once after another until you stop it
+/// * ekta jinis bar bar chote thaka
 
-// for of loops  start  <br>
+//? for of loops  start 
 
 //
-
-
-----------------------------------------------------------
-
 const numbers = [1, 3, 5, 8, 9] // arry variable 
 // const num // another singleVariable 
 for(const num of numbers){ // protome for e dokbe , then numbers array te , sekan theke single element 1st theke nibe  & show korbe , nibe & show korbe , jotokon na array ses ho
@@ -20,8 +14,7 @@ for(const num of numbers){ // protome for e dokbe , then numbers array te , seka
 }
 //
 
-----------------------------------------------------------
-----------------------------------------------------------
+
 /// another example 
 
 const fruits = ["A", "B" , "C", "D" , "E"] // until the array element end , the loop  will continued 
@@ -30,11 +23,8 @@ console.log(fruit); // fruits theke ekta ekta kore fruit nea seta dekabe
 console.log("I want to read")
 }
 
-----------------------------------------------------------
 // sumup : array modde protekta upadan ke loop caliea amra dekte pari .
 /// this is the usage of for of loop ,
 /// use array and singleconst variable
-//? for of loops  end /// need array and single variable
 
-        Know the for of loops  <End>
-=================================================
+//? for of loops  end /// need array and single variable
